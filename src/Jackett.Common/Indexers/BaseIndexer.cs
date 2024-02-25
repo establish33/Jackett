5a3806506de0063104e8ae1e2d012e4785f71d32
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-02-25 03:35:42 +0200

    core: remove automapper
    Co-authored-by: ngosang <ngosang@hotmail.es>

--- src/Jackett.Common/Indexers/BaseIndexer.cs
+++ src/Jackett.Common/Indexers/BaseIndexer.cs
@@ -633,7 +633,7 @@ namespace Jackett.Common.Indexers
                     break;
                 }
 
-                await DoFollowIfRedirect(response, referrer, overrideRedirectUrl, overrideCookies, accumulateCookies);
+                response = await DoFollowIfRedirect(response, referrer, overrideRedirectUrl, overrideCookies, accumulateCookies);
 
                 if (accumulateCookies)
                 {
@@ -673,7 +673,7 @@ namespace Jackett.Common.Indexers
             }
         }
 
-        private async Task DoFollowIfRedirect(WebResult incomingResponse, string referrer = null, string overrideRedirectUrl = null, string overrideCookies = null, bool accumulateCookies = false)
+        private async Task<WebResult> DoFollowIfRedirect(WebResult incomingResponse, string referrer = null, string overrideRedirectUrl = null, string overrideCookies = null, bool accumulateCookies = false)
         {
             if (incomingResponse.IsRedirect)
             {
@@ -694,8 +694,11 @@ namespace Jackett.Common.Indexers
                     Cookies = redirRequestCookies,
                     Encoding = Encoding
                 });
-                MapperUtil.Mapper.Map(redirectedResponse, incomingResponse);
+
+                return redirectedResponse;
             }
+
+            return incomingResponse;
         }
 
         protected List<string> GetAllTrackerCategories() =>
